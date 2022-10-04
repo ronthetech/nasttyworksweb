7cc6caa298dfb8b21a2fb68120a6063ab0c55372
ronthetech <rjfrancoisjr@icloud.com>
2022-10-04 00:06:01 -0400

    add hero to home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,16 @@
 import type { NextPage } from "next";
 import Head from "next/head";
-import Image from "next/image";
-import styles from "../styles/Home.module.css";
+import Hero from "../components/sections/Hero";
 
 const Home: NextPage = () => {
   return (
     <div>
       <Head>
         <title>Nastty Works</title>
-        <meta name="description" content="astty Works Web" />
-        <link rel="icon" href="/favicon.ico" />
+        <meta name="description" content="Nastty Works Home" />
       </Head>
 
-      <main className={styles.main}>
-        <h1>Home</h1>
-      </main>
+      <Hero />
     </div>
   );
 };
