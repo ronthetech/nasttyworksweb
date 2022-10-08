25f180c56f216fdf54bf1b16d15887d569857e2c
ronthetech <rjfrancoisjr@icloud.com>
2022-10-07 20:20:17 -0400

    update hero section

--- components/sections/Hero.tsx
+++ components/sections/Hero.tsx
@@ -1,25 +1,15 @@
-import { Fragment } from "react";
-import { Popover, Transition } from "@headlessui/react";
-import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
 import Link from "next/link";
-
-const navigation = [
-  { name: "Product", href: "#" },
-  { name: "Features", href: "#" },
-  { name: "Marketplace", href: "#" },
-  { name: "Company", href: "#" },
-];
 
 export default function Hero() {
   return (
     <div className="relative overflow-hidden bg-white">
       <div className="mx-auto max-w-7xl">
         <div className="relative z-10 bg-white pb-8 sm:pb-16 md:pb-20 lg:w-full lg:max-w-2xl lg:pb-28 xl:pb-32">
-          <main className="mx-auto mt-10 max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
+          <div className="mx-auto mt-10 max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
             <div className="sm:text-center lg:text-left">
               <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-5xl md:text-6xl">
-                <span className="block xl:inline">Mobile</span>{" "}
-                <span className="block text-indigo-600 xl:inline">
+                <span className="block xl:inline text-center">Mobile</span>{" "}
+                <span className="block text-teal-600 xl:inline text-center">
                   Mechanic
                 </span>
               </h1>
@@ -31,21 +21,21 @@ export default function Hero() {
               <div className="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
                 <div className="rounded-md shadow">
                   <Link href="/contact">
-                    <a className="flex w-full items-center justify-center rounded-md border border-transparent bg-indigo-600 px-8 py-3 text-base font-medium text-white hover:bg-indigo-700 md:py-4 md:px-10 md:text-lg">
+                    <a className="flex w-full items-center justify-center rounded-md border border-transparent bg-teal-600 px-8 py-3 text-lg font-medium text-white hover:bg-teal-700 md:py-4 md:px-10 md:text-xl">
                       Get an Estimate
                     </a>
                   </Link>
                 </div>
                 <div className="mt-3 sm:mt-0 sm:ml-3">
                   <Link href="/services">
-                    <a className="flex w-full items-center justify-center rounded-md border border-transparent bg-indigo-100 px-8 py-3 text-base font-medium text-indigo-700 hover:bg-indigo-200 md:py-4 md:px-10 md:text-lg">
+                    <a className="flex w-full items-center justify-center rounded-md border border-transparent bg-teal-100 px-8 py-3 text-lg font-medium text-teal-700 hover:bg-sky-200 md:py-4 md:px-10 md:text-xl">
                       Services We Offer
                     </a>
                   </Link>
                 </div>
               </div>
             </div>
-          </main>
+          </div>
         </div>
       </div>
       <div className="lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2"></div>
