86765c7ec0cf53b3fde2f9fa4aa78952fc1e0b2b
ronthetech <rjfrancoisjr@icloud.com>
2022-10-07 20:18:19 -0400

    add google font

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,7 +4,18 @@ import Script from "next/script";
 export default function Document() {
   return (
     <Html lang="en" className="h-full bg-gray-200">
-      <Head />
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="true"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Permanent+Marker&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
       <body className="h-full">
         <Main />
         <NextScript />
