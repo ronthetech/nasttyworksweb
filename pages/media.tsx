3273142847645b886dab20e846fba1f9b9fb8513
ronthetech <rjfrancoisjr@icloud.com>
2022-10-07 20:21:45 -0400

    add media page

---
+++ pages/media.tsx
@@ -0,0 +1,18 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+//import Content from "../components/sections/Content";
+
+const Media: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>Nastty Works Media</title>
+        <meta name="description" content="Nastty Works Media" />
+      </Head>
+
+      <div>{/* <Content /> */}</div>
+    </div>
+  );
+};
+
+export default Media;
