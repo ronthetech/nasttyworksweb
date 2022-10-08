6f151a1605f541ba422faafbbff923dc9d3886ce
ronthetech <rjfrancoisjr@icloud.com>
2022-10-07 20:20:31 -0400

    update layout

--- components/layouts/StackedLayout.tsx
+++ components/layouts/StackedLayout.tsx
@@ -2,6 +2,7 @@ import { ReactNode } from "react";
 import { Disclosure } from "@headlessui/react";
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
 import { useRouter } from "next/router";
+import Footer from "../sections/Footer";
 
 interface PrimaryLayoutProps {
   children: ReactNode;
@@ -12,6 +13,7 @@ const navigation = [
   { name: "Services", href: "/services", current: false },
   { name: "Contact", href: "/contact", current: false },
   // { name: "Media", href: "/media", current: false },
+  // { name: "Shop", href: "/shop", current: false },
 ];
 
 function classNames(...classes: string[]) {
@@ -26,15 +28,15 @@ export default function StackedLayout({ children }: PrimaryLayoutProps) {
         <Disclosure as="nav" className="bg-gray-800">
           {({ open }) => (
             <>
-              <div className="mx-auto max-w-xl px-4 sm:px-6 lg:px-8">
+              <div className="mx-auto max-w-sm px-4 sm:px-6 lg:px-6">
                 <div className="flex h-16 items-center justify-between">
                   <div className="flex items-center">
                     <div className="flex-shrink-0">
-                      <img
+                      {/* <img
                         className="h-8 w-8"
                         src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
                         alt="Nastty Works"
-                      />
+                      /> */}
                     </div>
 
                     <div className="hidden md:block">
@@ -110,14 +112,17 @@ export default function StackedLayout({ children }: PrimaryLayoutProps) {
 
         <header className="bg-white shadow">
           <div className="mx-auto max-w-7xl py-6 px-4 sm:px-6 lg:px-8">
-            <h1 className="text-3xl font-bold tracking-tight text-gray-900">
+            <h1
+              id="header-title"
+              className="text-5xl lg:text-7xl font-bold tracking-tight text-center text-teal-600"
+            >
               Nastty Works
             </h1>
           </div>
         </header>
-        <main>
+        <main className="min-h-full">
           <div className="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
-            <div className="px-4 py-6 sm:px-0">
+            <div className="px-4 lg:py-6 sm:px-0 py-0">
               {children}
               {/* <div className="h-96 rounded-lg border-4 border-dashed border-gray-500">
               </div> */}
@@ -125,6 +130,7 @@ export default function StackedLayout({ children }: PrimaryLayoutProps) {
           </div>
         </main>
       </div>
+      <Footer />
     </>
   );
 }
