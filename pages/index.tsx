d0055c4204c7c4031153d674b55900cb9ddf7387
Ron Jean-Francois <rjfrancoisjr@icloud.com>
2022-10-17 10:45:16 +0100

    edit: update layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,19 @@
-import type { NextPage } from "next";
-import Head from "next/head";
-import Hero from "../components/sections/Hero";
+import type { NextPage } from "next"
+import Head from "next/head"
+import Hero from "../components/sections/Hero"
 
 const Home: NextPage = () => {
-  return (
-    <div>
-      <Head>
-        <title>Nastty Works Auto</title>
-        <meta name="description" content="Nastty Works Home" />
-      </Head>
-      <Hero />
-    </div>
-  );
-};
+	return (
+		<div>
+			<>
+				<Head>
+					<title>Nastty Works Auto</title>
+					<meta name="description" content="Nastty Works Home" />
+				</Head>
+				<Hero />
+			</>
+		</div>
+	)
+}
 
-export default Home;
+export default Home
