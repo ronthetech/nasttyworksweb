2dfd393e191097178da09c692697e5e53ea88ff7
Ron Jean-Francois <rjfrancoisjr@icloud.com>
2022-12-05 15:15:00 -0500

    remove header shadow

--- components/layouts/StackedLayout.tsx
+++ components/layouts/StackedLayout.tsx
@@ -5,121 +5,121 @@ import { useRouter } from "next/router";
 import Footer from "../sections/Footer";
 
 interface PrimaryLayoutProps {
-  children: ReactNode;
+	children: ReactNode;
 }
 
 const navigation = [
-  { name: "Home", href: "/", current: true },
-  { name: "Services", href: "/services", current: false },
-  { name: "Contact", href: "/contact", current: false },
-  // { name: "Media", href: "/media", current: false },
-  // { name: "Shop", href: "/shop", current: false },
+	{ name: "Home", href: "/", current: true },
+	{ name: "Services", href: "/services", current: false },
+	{ name: "Contact", href: "/contact", current: false },
+	// { name: "Media", href: "/media", current: false },
+	// { name: "Shop", href: "/shop", current: false },
 ];
 
 function classNames(...classes: string[]) {
-  return classes.filter(Boolean).join(" ");
+	return classes.filter(Boolean).join(" ");
 }
 
 export default function StackedLayout({ children }: PrimaryLayoutProps) {
-  const router = useRouter();
-  return (
-    <>
-      <Disclosure as="nav" className="bg-gray-800">
-        {({ open }) => (
-          <>
-            <div className="mx-auto max-w-sm px-4 sm:px-6 lg:px-6">
-              <div className="flex h-16 items-center justify-between">
-                <div className="flex items-center">
-                  <div className="flex-shrink-0">
-                    {/* <img
+	const router = useRouter();
+	return (
+		<>
+			<Disclosure as="nav" className="bg-gray-800">
+				{({ open }) => (
+					<>
+						<div className="mx-auto max-w-sm px-4 sm:px-6 lg:px-6">
+							<div className="flex h-16 items-center justify-between">
+								<div className="flex items-center">
+									<div className="flex-shrink-0">
+										{/* <img
                         className="h-8 w-8"
                         src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
                         alt="Nastty Works"
                       /> */}
-                  </div>
+									</div>
 
-                  <div className="hidden md:block">
-                    <div className="ml-10 flex items-baseline space-x-4">
-                      {navigation.map((item) => (
-                        <a
-                          key={item.name}
-                          id={item.href}
-                          href={item.href}
-                          className={classNames(
-                            router.pathname == item.href
-                              ? "bg-gray-900 text-white"
-                              : "text-gray-300 hover:bg-gray-700 hover:text-white",
-                            "px-3 py-2 rounded-md text-sm font-medium"
-                          )}
-                          aria-current={
-                            router.pathname == item.href ? "page" : undefined
-                          }>
-                          {item.name.toUpperCase()}
-                        </a>
-                      ))}
-                    </div>
-                  </div>
-                </div>
+									<div className="hidden md:block">
+										<div className="ml-10 flex items-baseline space-x-4">
+											{navigation.map((item) => (
+												<a
+													key={item.name}
+													id={item.href}
+													href={item.href}
+													className={classNames(
+														router.pathname == item.href
+															? "bg-gray-900 text-white"
+															: "text-gray-300 hover:bg-gray-700 hover:text-white",
+														"px-3 py-2 rounded-md text-sm font-medium",
+													)}
+													aria-current={
+														router.pathname == item.href ? "page" : undefined
+													}>
+													{item.name.toUpperCase()}
+												</a>
+											))}
+										</div>
+									</div>
+								</div>
 
-                <div className="-mr-2 flex md:hidden">
-                  {/* Mobile menu button */}
-                  <Disclosure.Button className="inline-flex items-center justify-center rounded-md bg-gray-800 p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
-                    <span className="sr-only">Open main menu</span>
-                    {open ? (
-                      <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
-                    ) : (
-                      <Bars3Icon className="block h-6 w-6" aria-hidden="true" />
-                    )}
-                  </Disclosure.Button>
-                </div>
-              </div>
-            </div>
+								<div className="-mr-2 flex md:hidden">
+									{/* Mobile menu button */}
+									<Disclosure.Button className="inline-flex items-center justify-center rounded-md bg-gray-800 p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
+										<span className="sr-only">Open main menu</span>
+										{open ? (
+											<XMarkIcon className="block h-6 w-6" aria-hidden="true" />
+										) : (
+											<Bars3Icon className="block h-6 w-6" aria-hidden="true" />
+										)}
+									</Disclosure.Button>
+								</div>
+							</div>
+						</div>
 
-            <Disclosure.Panel className="md:hidden">
-              <div className="space-y-1 px-2 pt-2 pb-3 sm:px-3">
-                {navigation.map((item) => (
-                  <Disclosure.Button
-                    key={item.name}
-                    as="a"
-                    id={item.href}
-                    href={item.href}
-                    className={classNames(
-                      router.pathname == item.href
-                        ? "bg-gray-900 text-white"
-                        : "text-gray-300 hover:bg-gray-700 hover:text-white",
-                      "block px-3 py-2 rounded-md text-base font-medium"
-                    )}
-                    aria-current={
-                      router.pathname == item.href ? "page" : undefined
-                    }>
-                    {item.name}
-                  </Disclosure.Button>
-                ))}
-              </div>
-            </Disclosure.Panel>
-          </>
-        )}
-      </Disclosure>
+						<Disclosure.Panel className="md:hidden">
+							<div className="space-y-1 px-2 pt-2 pb-3 sm:px-3">
+								{navigation.map((item) => (
+									<Disclosure.Button
+										key={item.name}
+										as="a"
+										id={item.href}
+										href={item.href}
+										className={classNames(
+											router.pathname == item.href
+												? "bg-gray-900 text-white"
+												: "text-gray-300 hover:bg-gray-700 hover:text-white",
+											"block px-3 py-2 rounded-md text-base font-medium",
+										)}
+										aria-current={
+											router.pathname == item.href ? "page" : undefined
+										}>
+										{item.name}
+									</Disclosure.Button>
+								))}
+							</div>
+						</Disclosure.Panel>
+					</>
+				)}
+			</Disclosure>
 
-      <header className="bg-white shadow">
-        <div className="mx-auto max-w-7xl py-6 px-4 sm:px-6 lg:px-8">
-          <h1
-            id="header-title"
-            className="text-4xl lg:text-7xl font-bold tracking-tight text-center text-teal-600">
-            Nastty Works Auto
-          </h1>
-        </div>
-      </header>
-      <main className="min-h-full">
-        <div className="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
-          <div className="px-4 lg:py-6 sm:px-0 py-0">
-            {children}
-            {/* <div className="h-96 rounded-lg border-4 border-dashed border-gray-500">
+			<header className="bg-white">
+				<div className="mx-auto max-w-7xl py-6 px-4 sm:px-6 lg:px-8">
+					<h1
+						id="header-title"
+						className="text-4xl lg:text-7xl font-bold tracking-tight text-center text-teal-600">
+						Nastty Works Auto
+					</h1>
+				</div>
+			</header>
+			<main className="min-h-full">
+				<div className="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
+					<div className="px-4 lg:py-6 sm:px-0 py-0">
+						{children}
+						{/* <div className="h-96 rounded-lg border-4 border-dashed border-gray-500">
               </div> */}
-          </div>
-        </div>
-      </main>
-      <Footer />
-    </>
-  );
+					</div>
+				</div>
+			</main>
+			<Footer />
+		</>
+	);
 }
